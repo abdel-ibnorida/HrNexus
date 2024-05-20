9eedb75ad8ff2c24ba1d340d0f06f26cbbe306a1
Elenuccia03 <elenaisabellaichim89@gmail.com>
2024-05-20 18:06:31 +0200

    Commit login e calendario

---
+++ Controllers/CalendarioLavoratoriController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HrNexus.Controllers
+{
+    public class CalendarioLavoratoriController : Controller
+    {
+         public CalendarioLavoratoriController ()
+
+    {
+
+    }
+         public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }   
+    }
+}
